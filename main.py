a392ceea6199dabf0041b9c8b660e65d5d3d9433
JerryJack121 <73692359+JerryJack121@users.noreply.github.com>
2020-11-19 11:43:09 +0800

    Update

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import os
 import librosa
+from wav_plot import Wav_plot
 
 
 def main():
-    audio_path = ''
-    audio_name = '' 
+    audio_path = 'D:\DATASET\鳥叫聲\白頭翁'
+    audio_name = '國立臺北科技大學29.wav'
+    sig, sr = librosa.load(os.path.join(audio_path, audio_name))
 
-def load_audio(audio_path,audio_name):
-    sig, sr = librosa.load(os.path.join(audio_path,audio_name)) 
+    wav_plot = Wav_plot(sig, sr, audio_name)
+
+    wav_plot.time_wave()
 
 
 if __name__ == "__main__":
