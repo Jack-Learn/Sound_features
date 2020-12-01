a24ca25fe66a7c99b455e34278227ea1edeb80df
JerryJack121 <73692359+JerryJack121@users.noreply.github.com>
2020-12-01 13:06:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import librosa
 from wav_plot import Wav_plot
 import matplotlib.pyplot as plt
 from scipy import signal
+import soundfile as sf
 
 
 def main():
@@ -13,17 +14,18 @@ def main():
     sig, sr = librosa.load(os.path.join(audio_path, audio_name),
                            sr=40000,
                            duration=5)
+    audio = Wav_plot(sig, sr, audio_name)
+    # 初始化參數
+    audio1 = Wav_plot(sig, sr, audio_name)
+
     ######################################## 濾波器 #######################################################
 
     fx = 1024  #濾波器頻率
     wn = 2 * fx / sr
     b, a = signal.butter(8, wn, 'highpass')  #配置濾波器 8 表示濾波器的階數
-    sig = signal.filtfilt(b, a, sig)  #data為要過濾的訊號
-
-    ####################################### 初始化參數 ####################################################
-
+    sig2 = signal.filtfilt(b, a, sig)  #data為要過濾的訊號
     # 初始化參數
-    audio = Wav_plot(sig, sr, audio_name)
+    audio2 = Wav_plot(sig2, sr, audio_name)
 
     ################################### data augmentation #################################################
     # 對聲音進行擴增
@@ -40,12 +42,18 @@ def main():
     ####################################################################################################
 
     ## 畫圖
-    audio.Mel_spec(fmax=sr / 2)  #fmax跟採樣頻率有關，若fmax提高。採樣頻率也要提高，否則高頻會被切掉
+    audio1.Mel_spec(fmax=sr / 2)  #fmax跟採樣頻率有關，若fmax提高。採樣頻率也要提高，否則高頻會被切掉
+    audio2.Mel_spec(fmax=sr / 2)
     # audio.frequence_wavform()
     # ts.Mel_spec(fmax=sr / 2)
     # ps.Mel_spec(fmax=sr / 2)
     # audio.Mel_spec(fmax=sr / 2, augment=True)
     plt.show()
+
+    ######################################################################################################
+    # 輸出wav
+    # sf.write(os.path.join(audio_path, '濾波.wav'), sig2, sr)
+    ####################################### 初始化參數 ####################################################
 
 
 if __name__ == "__main__":
