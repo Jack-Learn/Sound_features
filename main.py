e8205b472cf02dd18fc29d64790ee3acc53b0dbd
JerryJack121 <73692359+JerryJack121@users.noreply.github.com>
2020-12-01 11:12:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ import os
 import librosa
 from wav_plot import Wav_plot
 import matplotlib.pyplot as plt
+from scipy import signal
 
 
 def main():
-    audio_path = 'D:\DATASET\咳嗽聲'
-    audio_name = '增耿.wav'
+    audio_path = 'D:\DATASET\冷氣故障聲'
+    audio_name = '國立臺北科技大學31.wav'
 
     # load audio
-    sig, sr = librosa.load(os.path.join(audio_path, audio_name), sr=30000)
+    sig, sr = librosa.load(os.path.join(audio_path, audio_name), sr=10000)
+    ####################################################################################################
+    #濾波器
+    # fx = 512  #濾波器頻率
+    # wn = 2 * fx / sr
+    # b, a = signal.butter(8, wn, 'highpass')  #配置濾波器 8 表示濾波器的階數
+    # sig = signal.filtfilt(b, a, sig)  #data為要過濾的訊號
+    ####################################################################################################
     # 初始化參數
     audio = Wav_plot(sig, sr, audio_name)
-
     ####################################################################################################
     # data augmentation
     # 對聲音進行擴增
@@ -27,11 +34,11 @@ def main():
     ####################################################################################################
 
     ## 畫圖
-    # audio.Mel_spec(fmax=sr / 2)  #fmax跟採樣頻率有關，若fmax提高。採樣頻率也要提高，否則高頻會被切掉
+    audio.Mel_spec(fmax=sr / 2)  #fmax跟採樣頻率有關，若fmax提高。採樣頻率也要提高，否則高頻會被切掉
+    # audio.frequence_wavform()
     # ts.Mel_spec(fmax=sr / 2)
     # ps.Mel_spec(fmax=sr / 2)
-    audio.Mel_spec(fmax=sr / 2, augment=True)
-
+    # audio.Mel_spec(fmax=sr / 2, augment=True)
     plt.show()
 
 
